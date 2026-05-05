342e6a16ad0bd3f3bb9a49c6870249f7dd711667
Serafini <giacomoserafini326@gmail.com>
2026-05-05 11:51:20 +0200

    add book adding form

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
 import { useState } from 'react'
 import './App.css'
 import './Filtri.css'
-import BooksGrid from '../src/booksGrid'
+import BooksGrid from './booksGrid'
+import AddBook from './addBook';
 
 function App() {
 
   console.log("APP()");
 
   return (
-    <>
-      <h1>Libreria:</h1>
-
-      <div className='filtri'>
-        <p>titolo</p>
-        <p>autore</p>
-        <p>genere</p>
-        <p>anno</p>
-        <input type="text" placeholder="Death in the clouds.."></input>
-        <input type="text" placeholder="Agatha Christie.."></input>
-        <input type="text" placeholder="Giallo.."></input>
-        <input type="text" placeholder="1935.."></input>
-      </div>
-
-      <BooksGrid />
-    </>
+    <div className='pagina'>
+      <header className='header'>
+        <h1>Libreria:</h1>
+        <h2>FILTRI</h2>
+        <div className='filtri'>
+          <p>titolo</p>
+          <p>autore</p>
+          <p>genere</p>
+          <p>anno</p>
+          <input type="text" placeholder="Death in the clouds.."></input>
+          <input type="text" placeholder="Agatha Christie.."></input>
+          <input type="text" placeholder="Giallo.."></input>
+          <input type="text" placeholder="1935.."></input>
+        </div>
+      </header>
+      <main className='main'>
+        <AddBook />
+        <BooksGrid />
+      </main>
+      <footer className='footer'>
+        <h3>Serafini & Minta</h3>
+      </footer>
+    </div>
   )
 }
 
